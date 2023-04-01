3765d7ba39ee76a81d8df9cdf1adbcfa0d186e42
legendparvesh <79099310+legendparvesh@users.noreply.github.com>
2023-04-01 07:27:40 +0530

    image from scp

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, request, url_for, render_template
 from flask_pymongo import PyMongo
+import pymongo
 app=Flask(__name__)
 app.config['MONGO_URI']="mongodb://camnode:camnode@10.147.18.11:27017/?authSource=crimedb"
 mongo=PyMongo(app)
@@ -16,6 +17,11 @@ def index():
     </form>
     
     '''
+client=pymongo.MongoClient('mongodb://admin:admin@10.147.18.11:27017/')
+
+crimedb=client['crimedb']
+
+crime = crimedb["crime"]
 
 img = os.path.join('static', 'img')
 @app.route('/rickroll')
@@ -23,6 +29,22 @@ def home():
     os.system("scp raspi@192.168.205.244:/images/latest.jpg ./static/img/")
     file = os.path.join(img, 'latest.jpg')
     return render_template('image_render.html', image=file)
+def add():
+    response=request.form['approvalStatus']
+    crimedb.crime.insert_one({'approvalStatus':response})
+
+@app.route('/maps')
+def map():
+    markers = [
+        {
+            'lat': 12.986914,
+            'lon': 79.972192,
+            'popup': 'chennai'
+        },
+
+    ]
+    return render_template('map.html', markers=markers)
+
 
 if __name__=='__main__':
     app.run(debug=True)
